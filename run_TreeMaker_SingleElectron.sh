fa94ecea771af83df00f074ea9f3893347bf8b39
SohamBhattacharya <soham.elessar@gmail.com>
2020-09-17 02:42:42 +0200

    Minor changes.

--- run_TreeMaker_SingleElectron.sh
+++ run_TreeMaker_SingleElectron.sh
@@ -29,11 +29,26 @@
 
 
 
+#cmsRun EDAnalyzers/TreeMaker/python/ConfFile_cfg.py \
+#    sourceFile=\
+#sourceFiles/SingleElectron_PT2to200_Phase2HLTTDRWinter20DIGI-NoPU_110X_mcRun4_realistic_v3-v2_GEN-SIM-DIGI-RAW/SingleElectron_PT2to200_Phase2HLTTDRWinter20DIGI-NoPU_110X_mcRun4_realistic_v3-v2_GEN-SIM-DIGI-RAW.txt \
+#    genEleFilter=1 \
+#    genPartonFilter=0 \
+#    isGunSample=1 \
+#    onRaw=1 \
+#    maxEvents=1000 \
+
+
+
 cmsRun EDAnalyzers/TreeMaker/python/ConfFile_cfg.py \
     sourceFile=\
-sourceFiles/SingleElectron_PT2to200_Phase2HLTTDRWinter20DIGI-NoPU_110X_mcRun4_realistic_v3-v2_GEN-SIM-DIGI-RAW/SingleElectron_PT2to200_Phase2HLTTDRWinter20DIGI-NoPU_110X_mcRun4_realistic_v3-v2_GEN-SIM-DIGI-RAW.txt \
+sourceFiles/SingleElectron_PT2to200_Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3_ext2-v2_GEN-SIM-DIGI-RAW/SingleElectron_PT2to200_Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3_ext2-v2_GEN-SIM-DIGI-RAW.txt \
     genEleFilter=1 \
     genPartonFilter=0 \
     isGunSample=1 \
+    modTICLele=0 \
+    modTICLeleWithRerunTICL=0 \
+    rerunTICL=0 \
     onRaw=1 \
-    maxEvents=1000 \
+    debugFile=0 \
+    maxEvents=10 \
